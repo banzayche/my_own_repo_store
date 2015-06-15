6b69b83ea83cac1ddc4240c72a0c9666e601fd50
banzayche <dharm.serg@gmail.com>
2015-06-15 15:36:07 +0300

    Step two: We created controller and sent some product data to DOM.

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+// reloading with gulp-connect server
+
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    watch = require('gulp-watch'),
+    batch = require('gulp-batch'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    csscomb = require('gulp-csscomb'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint');
+
+
+
+// ---------------CSS TASKS--------------------------------------------------------------------------
+gulp.task('cssconcatinate', function() {
+  gulp.src('public/**/*.css')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('public/css'));
+    
+    setTimeout(function(){
+        return gulp.run('csscombination');
+    }, 500);
+});
+
+gulp.task('csscombination', function () {
+  gulp.src('public/css/all.css')
+    .pipe(csscomb())
+    .pipe(gulp.dest('public/css'));
+
+    setTimeout(function(){
+        return gulp.run('cssminification');
+    }, 500);
+});
+
+gulp.task('cssminification', function () {
+    gulp.src('public/css/all.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('csstask', function(){
+    gulp.run('cssconcatinate');
+});
+
+
+
+// ---------------JS TASKS---------------------------------------------------------------------------
+gulp.task('hint', function() {
+  return gulp.src('public/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('concatinate', function() {
+    gulp.src('public/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('public/js'));
+    
+    setTimeout(function(){
+        return gulp.run('compress');
+    }, 500);
+});
+
+gulp.task('compress', function() {
+  return gulp.src('public/js/all.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('jstask', function(){
+    gulp.run('hint', 'concatinate');
+});
+
+
+
+// ---------------GULP WATCH---------------------------------------------------------------------------
+gulp.task('stream', function () {
+    watch('public/js/*.js', batch(function (events, done) {
+        gulp.start('jstask', done);        
+    }));
+
+    watch('public/css/*.css', batch(function (events, done) {
+        gulp.start('csstask', done);
+    }));
+
+    gulp.src('public/*.html')
+         .pipe(watch('public/*.**'))
+         .pipe(connect.reload());
+});
+
+
+// -------------------SERVER-LIVERELOAD-----------------------------------------------
+gulp.task('connect', function(){
+    connect.server({
+        root: 'public',
+        livereload: true
+    });    
+});
+
+
+// ---------------DEFAULT---------------------------------------------------------------------------
+gulp.task('default', ['stream', 'connect']);
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('store', [ ]);
+(function () {
+	// angular module
+	var app = angular.module('store', [ ]);
+	
+	// angular controller
+	app.controller('StoreController', function(){
+		// to declare new attribute "product" and create link to gem
+		this.product = gem;
+	});
+
+	// some data
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'The best Dodecahedron of the World!'
+	};
+})();
