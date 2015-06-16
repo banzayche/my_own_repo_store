1411ca0490ed165cbdfd9a24c4540307c5b6701f
banzayche <dharm.serg@gmail.com>
2015-06-16 12:57:01 +0300

    Studied to create Tabs vs Angular.js

--- public/js/app.js
+++ public/js/app.js
@@ -8,20 +8,68 @@
 		this.products = gems;
 	});
 
+	app.controller('GalleryController', function(){
+	  	this.current = 0;
+	    this.setCurrent = function(attr){
+	      if(attr){
+	      	this.current = attr;
+	      } else {
+	      	this.current = 0;
+	      }
+	    };
+	  });
+
+	// panel controller
+	app.controller('TabController', function(){
+		this.tab = 1;
+	    
+	    this.setTab = function(selectedTab){
+	      this.tab = selectedTab;
+	    };
+	    
+	    this.isSet = function(givenTab){
+	      return this.tab === givenTab;
+	    };
+	});
+
 	// some data
 	var gems = [
 		{
 			name: 'Octopen',
 			price: 12.95,
 			description: 'The best Octopen of the World!',
+			specefication: 'Some specefication of Octopen.',
 			canPurchase: false,
-			soldOut: false
+			soldOut: false,
+			images: [
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif",
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-05.gif",
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-09.gif"
+		      ],
 		}, {
 			name: 'Dodecahedron',
-			price: 2.95,
+			price: 32,
 			description: 'The best Dodecahedron of the World!',
+			specefication: 'Specefication of Dodecahedron.',
 			canPurchase: false,
-			soldOut: false
-		}
+			soldOut: false,
+			images: [
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif",
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-03.gif",
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-04.gif"
+		      ],
+		}, {
+			name: 'Collider',
+			price: 112.95,
+			description: 'The best Collider of the World!',
+			specefication: 'Wery nice Collider',
+			canPurchase: false,
+			soldOut: false,
+			images: [
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-06.gif",
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-07.gif",
+		        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-10.gif"
+		      ],
+		},
 	];
 })();
