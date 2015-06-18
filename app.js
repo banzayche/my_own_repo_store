10c3fbcf3d162a873014665552ccca4342b03a9c
banzayche <dharm.serg@gmail.com>
2015-06-18 14:39:20 +0300

    It's first step to creating more strong app vs Angular

---
+++ app.js
@@ -0,0 +1,97 @@
+// Подключаем модуль експресс
+var express = require('express');
+// Подключаем модуль боди-парсер
+var bodyParser = require('body-parser');
+// делаем переменную ссылкой на модуль экспресс
+var app = express();
+
+// Обращаемся к файлу tasks, который находится в этой же дирректории
+var books = require('./products');
+// присваеваем переменной то, что возвращает нам код в файле
+books = books.productsList();
+
+
+// переменная в которой происходит подсчет айди
+var nextId = books.length;
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    if(req.url.indexOf("/api") === 0 ||
+        req.url.indexOf("/bower-components") === 0 ||
+        req.url.indexOf("/scripts") === 0) {
+        return next();
+    }
+
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/books', function(req, res) {
+    res.json(books);
+});
+
+// app.get('/api/books/:id', function(req, res) {
+//     var book = books.filter(function(book) { return book.id == req.params.id; })[0];
+
+//     if(!book) {
+//         res.statusCode = 404;
+//         return res.json({ msg: "Book does not exist" });
+//     }
+
+//     res.json(book);
+// });
+
+// app.post('/api/books', function(req, res) {
+//     if(!req.body.author || !req.body.title) {
+//         res.statusCode = 400;
+//         return res.json({ msg: "Invalid params sent" });
+//     }
+
+//     var newBook = {
+//         author : req.body.author,
+//         title : req.body.title,
+//         year: req.body.year,
+//         genre: req.body.genre,
+//         description: req.body.description,
+//         id: nextId++
+//     };
+
+//     books.push(newBook);
+//     res.json(newBook);
+// });
+
+app.post('/api/books/:id/reviews', function(req, res) {
+    if(!req.body.author || !req.body.body) {
+        res.statusCode = 400;
+        return res.json({ msg: "Invalid params sent" });
+    }
+    var newId= req.params.id;
+
+    var newBook = {
+        stars : req.body.stars,
+        body : req.body.body,
+        createdOn: req.body.createdOn,
+        author: req.body.author,
+    };
+
+    books[newId].reviews.push(newBook);
+    res.json(newBook);
+});
+
+// app.delete('/api/books/:id', function(req, res) {
+//     var book = books.filter(function(book) { return book.id == req.params.id; })[0];
+
+//     if(!book) {
+//         res.statusCode = 404;
+//         return res.json({ msg: "Book does not exist" });
+//     }
+
+//     books.splice(books.indexOf(book), 1);
+
+//     res.statusCode = 204;
+//     res.send({});
+// });
+
+// адресс порта
+app.listen(8300);
+console.log('Server side has been started')
--- public/js/app.js
+++ public/js/app.js
@@ -7,95 +7,46 @@
 			var store = this;
 			store.products = products;
 
-			$http.get('/products.json').success(function(data){
+			$http.get('/api/books').success(function(data){
 				store.products = data;
 			});
 		}]);
 
 	// comment controller
-	app.controller('ReviewController', function(){
+	app.controller('ReviewController', ['$http', function($http){
 	    this.review = {};
 	    
 	    this.addReview = function(product){
+	    	var productId = product.id;
 	    	this.review.createdOn = Date.now();
-	    	console.log(this.review);
 	    	product.reviews.push(this.review);
-	      
-	      	this.review = {};
-	    };
-	  });
+	      	
+	      	$http.post('/api/books/'+productId+'/reviews', this.review).success(function(data){
+				console.log('Review is saved successfully! Pa-ra-ram-pam-pam:)');
+			});
 
-	// some data
-	// var gems = [
-	// 	{
-	// 		name: 'Octopen',
-	// 		price: 12.95,
-	// 		description: 'The best Octopen of the World!',
-	// 		specefication: 'Some specefication of Octopen.',
-	// 		canPurchase: false,
-	// 		soldOut: false,
-	// 		images: [
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-02.gif",
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-05.gif",
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-09.gif"
-	// 	      ],
-	// 	    reviews: [{
-	// 	        stars: 5,
-	// 	        body: "I love this gem!",
-	// 	        author: "joe@example.org",
-	// 	        createdOn: 1397490980837
-	// 	      }, {
-	// 	        stars: 1,
-	// 	        body: "This gem sucks.",
-	// 	        author: "tim@example.org",
-	// 	        createdOn: 1397490980837
-	// 	      }],
-	// 	}, {
-	// 		name: 'Dodecahedron',
-	// 		price: 32,
-	// 		description: 'The best Dodecahedron of the World!',
-	// 		specefication: 'Specefication of Dodecahedron.',
-	// 		canPurchase: false,
-	// 		soldOut: false,
-	// 		images: [
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-01.gif",
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-03.gif",
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-04.gif"
-	// 	      ],
-	// 	    reviews: [{
-	// 	        stars: 5,
-	// 	        body: "I love this gem!",
-	// 	        author: "joe@example.org",
-	// 	        createdOn: 1397490980837
-	// 	      }, {
-	// 	        stars: 1,
-	// 	        body: "This gem sucks.",
-	// 	        author: "tim@example.org",
-	// 	        createdOn: 1397490980837
-	// 	      }],
-	// 	}, {
-	// 		name: 'Collider',
-	// 		price: 112.95,
-	// 		description: 'The best Collider of the World!',
-	// 		specefication: 'Wery nice Collider',
-	// 		canPurchase: false,
-	// 		soldOut: false,
-	// 		images: [
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-06.gif",
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-07.gif",
-	// 	        "http://dhg7upb7j7jqa.cloudfront.net/shaping_up_with_angular_js/assets/demo/images/gem-10.gif"
-	// 	      ],
-	// 	    reviews: [{
-	// 	        stars: 5,
-	// 	        body: "I love this gem!",
-	// 	        author: "joe@example.org",
-	// 	        createdOn: 1397490980837
-	// 	      }, {
-	// 	        stars: 1,
-	// 	        body: "This gem sucks.",
-	// 	        author: "tim@example.org",
-	// 	        createdOn: 1397490980837
-	// 	      }],
-	// 	},
-	// ];
+	      	this.review = {};	      	
+	    };
+	  }]);
+
+
+// ---------------------------------
+// routing
+// ---------------------------------
+	// app.config(['$routerProvider', '$locationProvider', function ($routerProvider, $locationProvider){
+	// 	$routerProvider
+	// 		when('/', {
+	// 			controller: 'ListController',
+	// 			templateUrl: 'views/list.html'
+	// 		})
+	// 		when('/letter/:letter', {
+	// 			controller: 'LetterController',
+	// 			templateUrl: 'views/letter.html'
+	// 		})
+	// 	.otherwise({
+	// 		redirect: '/'
+	// 	});
+	// 	#locationProvider
+	// 		.html5Mode(false);
+	// }]);
 })();
